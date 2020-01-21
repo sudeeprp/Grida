e8846999dcb1d5e4ab5dad617bc9a1a6478db1e7
Sudeep <sudeep.rp@gmail.com>
2020-01-21 13:24:37 +0530

    Add support for Goa

--- app/src/main/java/com/thinklearn/tide/grida/TokenPro.kt
+++ app/src/main/java/com/thinklearn/tide/grida/TokenPro.kt
@@ -19,14 +19,27 @@ class TokenPro : AppCompatActivity() {
         if(intent.hasExtra(Intent.EXTRA_TEXT)) {
             caller_ident = intent.getStringExtra(Intent.EXTRA_TEXT)
         }
-        if(intent.action == Intent.ACTION_SEND && "text/plain" == intent.type && "rfgt\$%UJ" == caller_ident) {
+        var credentialFilename = ""
+        var UID = ""
+        if("rfgt\$%UJ" == caller_ident) {
+            credentialFilename = "icgrid-6540f-859a57f482d3.json"
+            UID = "ICGridActiv"
+        } else if("slwk-ga" == caller_ident) {
+            credentialFilename = "gagrid-e25d0-4c1353152fa6.json"
+            UID = "GAGridActiv"
+        } else {
+            Toast.makeText(this, "Caller project unknown!", Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+        if(intent.action == Intent.ACTION_SEND && "text/plain" == intent.type) {
             // Initialize FirebaseApp only when not already initialized.
             try {
                 FirebaseApp.getInstance()
                 Toast.makeText(this, "Got instance", Toast.LENGTH_SHORT).show()
             } catch (ex: IllegalStateException) {
                 try {
-                    val inputStream = assets.open("icgrid-6540f-859a57f482d3.json")
+                    val inputStream = assets.open(credentialFilename)
                     Toast.makeText(this, "building...", Toast.LENGTH_SHORT).show()
                     val options = FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(inputStream)).
                             build()
@@ -38,7 +51,6 @@ class TokenPro : AppCompatActivity() {
                 }
             }
 
-            val UID = "ICGridActiv"
             Toast.makeText(this, "creating custom token...", Toast.LENGTH_SHORT).show()
             val customToken: String = FirebaseAuth.getInstance().createCustomTokenAsync(UID).get()
             Toast.makeText(this, "created!", Toast.LENGTH_SHORT).show()
